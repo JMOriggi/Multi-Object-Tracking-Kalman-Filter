2f56effea1170a2a97dd461673e699a42d15ae71
JMOriggi <36966882+JMOriggi@users.noreply.github.com>
2021-05-10 18:46:01 -0400

    Update App_bats_tracking.py

--- App_bats_tracking.py
+++ App_bats_tracking.py
@@ -1,19 +1,3 @@
-"""
- Kalman filtering, also known as linear quadratic estimation (LQE), is an algorithm 
- that uses a series of measurements observed over time, containing statistical 
- noise and other inaccuracies, and produces estimates of unknown variables that tend 
- to be more accurate than those based on a single measurement alone, by estimating 
- a joint probability distribution over the variables for each timeframe.
- 
- Essentially the measurement has some noise and with the kalman filter we want ot eliminate the noise.
- 
-Kalman provide an estimate of want I cannot measure precisely with what I can measure at the moment.
-Also use to fuse multiple noisy measurement to best find the car position for example.
-
-Kalman is a linear model, so good for object that moves in a linear way (like bats). Cells for example moves in a non linear way,
-in that case we should consider other methods (complex non linear Kalman filter).
-"""
-
 import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
